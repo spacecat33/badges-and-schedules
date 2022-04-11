dea8605779e1e36f812d6f3181f7a7a581ae3379
Janan Akkad <jananakkad@hotmail.co.uk>
2022-04-11 11:36:13 +0100

    Completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,56 @@
-# Write your code here.
+#First task
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.map {|attendee| "Hello, my name is #{attendee}."}
+end
+#or
+def batch_badge_creator(attendees)
+    messages = []
+    attendees.each do |attendee|
+        messages << "Hello, my name is #{attendee}."
+    end
+    return messages
+end
+
+#Second task
+def assign_rooms(attendees)
+    badgeArray = []
+    room_assignments = 1
+    attendees.each do |attendee|
+        badgeArray << "Hello, #{attendee}! You'll be assigned to room #{room_assignments}!"
+        room_assignments += 1
+    end
+    badgeArray
+end
+#or
+def assign_rooms(attendees)
+    badgeArray = []
+    attendees.each_with_index do |attendee, room_assignment|
+        badgeArray << "Hello, #{attendee}! You'll be assigned to room #{room_assignment + 1}!"
+    end
+    return badgeArray
+end
+
+#Third task
+def printer(attendees)
+    attendees.map do |attendee| 
+        puts "Hello, my name is #{attendee}."
+    end
+    room_assignments = 1
+    attendees.each do |attendee|
+        puts "Hello, #{attendee}! You'll be assigned to room #{room_assignments}!"
+        room_assignments += 1
+    end
+end
+#or
+def printer(attendees)
+    batch_badge_creator(attendees).each do |attendee|
+        puts attendee
+    end
+    assign_rooms(attendees).each do |room_assignment|
+        puts room_assignment
+    end
+end
